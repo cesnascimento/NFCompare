b55525db7154a1f0d7645826cd03f72ddff51f02
Carlos Nascimento <cnascimento@dermage.com.br>
2022-12-01 14:59:15 -0300

    Script rodando agora em documentos XML

--- nfcompare.py
+++ nfcompare.py
@@ -1,55 +1,81 @@
-import tabula
-import re
 import os
+import locale
+from bs4 import BeautifulSoup
 
 
-def open_file():
-    lista_tabelas = tabula.read_pdf('nfe 105 refe 6361.pdf', pages='all')
-    coluna_cnpj = lista_tabelas[1].iloc[15].astype(str)[6]
-    return coluna_cnpj
+locale.setlocale(locale.LC_MONETARY, '')
 
 
-def get_cnpj(cnpj):
-    cnpj = re.sub(r'\d{2}/\d{2}/\d{4}', '', cnpj)
+def open_nfd():
+    file = open('1200010006361.xml')
+    return file
+
+
+def get_cnpj_ref(file):
+    soup = BeautifulSoup(file, 'xml')
+    cnpj = soup.find('CNPJ')
     return cnpj
 
 
-def get_items():
-    lista_tabelas = tabula.read_pdf('nfe 105 refe 6361.pdf', pages='all')
-    #coluna_cnpj = lista_tabelas[1].iloc[15].astype(str)[6]
-    item, qtd = lista_tabelas[1].iloc[39].astype(
-        str)[0], lista_tabelas[1].iloc[39].astype(str)[6]
-    item1, qtd1 = lista_tabelas[1].iloc[40].astype(
-        str)[0], lista_tabelas[1].iloc[40].astype(str)[6]
-    item2, qtd2 = lista_tabelas[1].iloc[41].astype(
-        str)[0], lista_tabelas[1].iloc[41].astype(str)[6]
-    print(item, qtd.strip()[0])
-    print(item1, qtd1.strip()[0])
-    print(item2, qtd2.strip()[0])
+def get_ref(file):
+    soup = BeautifulSoup(file, 'xml')
+    codigo_ref = soup.find('nNF').getText()
+    return codigo_ref
 
 
-def get_nfe_compare():
-    folders = os.listdir('\\\\dmsrv-dts/XML_Danfe')
-    cnpj = re.sub('[./-]', '', get_cnpj(open_file()))
+def get_info_products_nf(file):
+    products = dict()
+    soup = BeautifulSoup(file, 'xml')
+    name_products, code_products, unit_products, price_products = soup.findAll(
+        'xProd'), soup.findAll('cProd'), soup.findAll('qCom'), soup.findAll('vUnCom')
+    for name, code, unit, price in zip(name_products, code_products, unit_products, price_products):
+        name, code, unit, price = name.getText(), code.getText(), unit.getText(), price.getText()
+        price = locale.currency(float(price))
+        unit = int(float(unit))
+        products.update({name: [code,  unit, price]})
+    return products
+
+
+def get_folder_network(cnpj):
+    path = '\\\\dmsrv-dts/XML_Danfe/'
+    folders = os.listdir(path)
     for folder in folders:
         if folder in cnpj:
-            os.chdir(f'\\\\dmsrv-dts/XML_Danfe/{folder}/')
-            new_folder = os.getcwd()
-            if os.path.isdir(new_folder):
-                print(f'Pasta encontrada: {os.getcwd()}')
-                return os.getcwd()
-            else:
-                print(f'Pasta não encontrada!')
+            path = os.path.join(path, folder)
+            print(f'Pasta com o CNPJ encontrada: \\\\{path}')
+            return path
 
 
-def open_nfe_compare(folder):
-    print(folder)
+def get_xml_file(folders):
+    code_ref = get_ref(open_nfd())
+    for folder in folders:
+        if code_ref in folder and '.xml' in folder:
+            print(
+                f'Arquivo XML com o codigo de referencia encontrado: {folder}')
+            return folder
+
+
+def get_doc_ref(path):
+    folders = os.listdir(path)
+    file_xml = os.path.join(path, get_xml_file(folders))
+    if os.path.isfile(file_xml):
+        return file_xml
+
+
+def open_nfe(file_xml):
+    print(file_xml)
+    with open(file_xml) as f:
+        soup = BeautifulSoup(f'\\{f}', 'xml')
+        print('pagina aberta')
+    pass
+
 
 
 def start():
-    get_cnpj(open_file())
-    get_items()
-    open_nfe_compare(get_nfe_compare())
+    a = get_doc_ref(get_folder_network(get_cnpj_ref(open_nfd())))
+    get_info_products_nf(open_nfd())
+    open_nfd().close()
+    open_nfe(a)
 
 
 start()
