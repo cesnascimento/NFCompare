97a4d2668efdb4452c981c8729928a640671b197
Carlos Nascimento <cnascimento@dermage.com.br>
2022-12-02 11:14:25 -0300

    NFCompare v0.1

--- nfcompare.py
+++ nfcompare.py
@@ -24,32 +24,33 @@ def get_code_ref(file):
     return code_ref
 
 
-'''def get_info_products_nf(file):
-    products = dict()
-    soup = BeautifulSoup(file, 'xml')
+def get_products_nfd(file):
+    products = list()
+    soup = BeautifulSoup(file, 'lxml')
     name_products, code_products, unit_products, price_products = soup.findAll(
-        'xProd'), soup.findAll('cProd'), soup.findAll('qCom'), soup.findAll('vUnCom')
+        'xprod'), soup.findAll('cprod'), soup.findAll('qcom'), soup.findAll('vuncom')
     for name, code, unit, price in zip(name_products, code_products, unit_products, price_products):
-        name, code, unit, price = name.getText(), code.getText(), unit.getText(), price.getText()
+        name, code, unit, price = name.getText(
+        ), code.getText(), unit.getText(), price.getText()
         price = locale.currency(float(price))
         unit = int(float(unit))
-        products.update({name: [code,  unit, price]})
+        products.append({'code': code, 'name': name,
+                        'unit': unit, 'price': price})
     return products
 
 
-def get_folder_network(cnpj):
+def open_folder_network(cnpj):
     path = '\\\\dmsrv-dts/XML_Danfe/'
     folders = os.listdir(path)
-    print(path)
     for folder in folders:
         if folder in cnpj:
             path = os.path.join(path, folder)
-            print(f'Pasta com o CNPJ encontrada: \\\\{path}')
+            print(f'Pasta com o CNPJ encontrada: {path}')
             return path
 
 
-def get_xml_file(folders):
-    code_ref = get_ref(open_nfd())
+def get_file_xml(folders):
+    code_ref = get_code_ref(open_nfd())
     for folder in folders:
         if code_ref in folder and '.xml' in folder:
             print(
@@ -57,28 +58,78 @@ def get_xml_file(folders):
             return folder
 
 
-def get_doc_ref(path):
+def find_file_xml(path):
     folders = os.listdir(path)
-    file_xml = os.path.join(path, get_xml_file(folders))
+    file_xml = os.path.join(path, get_file_xml(folders))
     if os.path.isfile(file_xml):
         return file_xml
+    else:
+        print('Arquivo XML não encontrado!')
 
 
-def open_nfe_compare(file_xml):
+def open_nfe_network(file_xml):
     file = open(file_xml)
     soup = BeautifulSoup(file, 'xml')
-    print(soup)
-    pass'''
+    cnpj_nfe = soup.find('CNPJ').getText()
+    return cnpj_nfe
 
+
+def cnpj_compare(cnpj_nfe):
+    if cnpj_nfe == get_cnpj_ref(open_nfd()):
+        print('CNPJ da nota de NFD corresponde ao CNPJ da NFE')
+
+
+def get_products_compare(file_xml):
+    products_compare = list()
+    file = open(file_xml)
+    soup = BeautifulSoup(file, 'xml')
+    name_products, code_products, unit_products, price_products = soup.findAll(
+        'xProd'), soup.findAll('cProd'), soup.findAll('qCom'), soup.findAll('vUnCom')
+    for name, code, unit, price in zip(name_products, code_products, unit_products, price_products):
+        name, code, unit, price = name.getText(
+        ), code.getText(), unit.getText(), price.getText()
+        price = locale.currency(float(price))
+        unit = int(float(unit))
+        products_compare.append(
+            {'code': code, 'name': name, 'unit': unit, 'price': price})
+    return products_compare
+
+
+def products_compare(products, products_compare):
+    for productA in products:
+        for productB in products_compare:
+            if productA['code'] != productB['code']:
+                pass
+            else:
+                if productA['code'] == productB['code']:
+                    print()
+                    print(
+                        f'''Produto encontrado nas duas NF: {productA['name']} ''')
+                    print(f'''Código: {productA['code']} ''')
+                    if productA['price'] == productB['price']:
+                        print(f'''Valor: {productA['price']} ''')
+                    else:
+                        print('O valor da NFD é menor que o da NFE')
+                        print(f'''O valor da NFD é de: {productA['price']}''')
+                        print(f'''O valor da NFE é de: {productB['price']}''')
+                    if productA['unit'] <= productB['unit']:
+                        print(f'''Quantidade devolvida: {productA['unit']} ''')
+                    else:
+                        print('A quantidade devolvida é maior')
+                        print(f'''A quantidade comprada foi: {productB['unit']}''')
+                        print(f'''A quantidade devolvida foi: {productA['unit']}''')
 
 
 def start():
-     get_cnpj_ref(open_nfd())
-     get_code_ref(open_nfd())
-'''a = get_doc_ref(get_folder_network(get_cnpj_ref(open_nfd())))
-    get_info_products_nf(open_nfd())
+    a = find_file_xml(open_folder_network(get_cnpj_ref(open_nfd())))
+    b = open_nfe_network(a)
+    cnpj_compare(b)
+    get_products_compare(a)
+    products_compare(get_products_nfd(open_nfd()), get_products_compare(a))
+    get_code_ref(open_nfd())
+    get_products_nfd(open_nfd())
+
     open_nfd().close()
-    open_nfe_compare(a)
-'''
+
 
 start()
--- teste1.py
+++ teste1.py
@@ -7,48 +7,50 @@ locale.setlocale(locale.LC_MONETARY, '')
 
 
 def open_nfd():
-    file = open('1200010006361.xml')
+    file = open('cosmoprime.xml')
     return file
 
 
 def get_cnpj_ref(file):
-    soup = BeautifulSoup(file, 'xml')
-    cnpj = soup.find('CNPJ').getText()
+    soup = BeautifulSoup(file, 'lxml')
+    cnpj = soup.findAll('cnpj')[1].getText()
     return cnpj
 
 
-def get_ref(file):
-    soup = BeautifulSoup(file, 'xml')
-    codigo_ref = soup.find('nNF').getText()
-    print(codigo_ref)
-    return codigo_ref
+def get_code_ref(file):
+    soup = BeautifulSoup(file, 'lxml')
+    code_ref = soup.find('refnfe').getText()
+    code_ref = code_ref[30:34]
+    return code_ref
 
 
-def get_info_products_nf(file):
-    products = dict()
-    soup = BeautifulSoup(file, 'xml')
+def get_products_nfd(file):
+    products = list()
+    soup = BeautifulSoup(file, 'lxml')
     name_products, code_products, unit_products, price_products = soup.findAll(
-        'xProd'), soup.findAll('cProd'), soup.findAll('qCom'), soup.findAll('vUnCom')
+        'xprod'), soup.findAll('cprod'), soup.findAll('qcom'), soup.findAll('vuncom')
     for name, code, unit, price in zip(name_products, code_products, unit_products, price_products):
-        name, code, unit, price = name.getText(), code.getText(), unit.getText(), price.getText()
+        name, code, unit, price = name.getText(
+        ), code.getText(), unit.getText(), price.getText()
         price = locale.currency(float(price))
         unit = int(float(unit))
-        products.update({name: [code,  unit, price]})
+        products.append({'code': code, 'name': name,
+                        'unit': unit, 'price': price})
     return products
 
 
-def get_folder_network(cnpj):
+def open_folder_network(cnpj):
     path = '\\\\dmsrv-dts/XML_Danfe/'
     folders = os.listdir(path)
     for folder in folders:
         if folder in cnpj:
             path = os.path.join(path, folder)
-            print(f'Pasta com o CNPJ encontrada: \\\\{path}')
+            print(f'Pasta com o CNPJ encontrada: {path}')
             return path
 
 
-def get_xml_file(folders):
-    code_ref = get_ref(open_nfd())
+def get_file_xml(folders):
+    code_ref = get_code_ref(open_nfd())
     for folder in folders:
         if code_ref in folder and '.xml' in folder:
             print(
@@ -56,26 +58,76 @@ def get_xml_file(folders):
             return folder
 
 
-def get_doc_ref(path):
+def find_file_xml(path):
     folders = os.listdir(path)
-    file_xml = os.path.join(path, get_xml_file(folders))
+    file_xml = os.path.join(path, get_file_xml(folders))
     if os.path.isfile(file_xml):
         return file_xml
+    else:
+        print('Arquivo XML não encontrado!')
 
 
-def open_nfe_compare(file_xml):
+def open_nfe_network(file_xml):
     file = open(file_xml)
     soup = BeautifulSoup(file, 'xml')
-    #print(soup)
+    cnpj_nfe = soup.find('CNPJ').getText()
+    return cnpj_nfe
+
+
+def cnpj_compare(cnpj_nfe):
+    if cnpj_nfe == get_cnpj_ref(open_nfd()):
+        print('CNPJ da nota de NFD corresponde ao CNPJ da NFE')
+
+
+def get_products_compare(file_xml):
+    products_compare = list()
+    file = open(file_xml)
+    soup = BeautifulSoup(file, 'xml')
+    name_products, code_products, unit_products, price_products = soup.findAll(
+        'xProd'), soup.findAll('cProd'), soup.findAll('qCom'), soup.findAll('vUnCom')
+    for name, code, unit, price in zip(name_products, code_products, unit_products, price_products):
+        name, code, unit, price = name.getText(
+        ), code.getText(), unit.getText(), price.getText()
+        price = locale.currency(float(price))
+        unit = int(float(unit))
+        products_compare.append(
+            {'code': code, 'name': name, 'unit': unit, 'price': price})
+    return products_compare
+
+
+def products_compare(products, products_compare):
+    for productA in products:
+        for productB in products_compare:
+            if productA['code'] == productB['code']:
+                print()
+                print(
+                    f'''Produto encontrado nas duas NF: {productA['name']} ''')
+                print(f'''Código: {productA['code']} ''')
+                if productA['price'] == productB['price']:
+                    print(f'''Valor: {productA['price']} ''')
+                else:
+                    print('O valor da NFD é menor que o da NFE')
+                    print(f'''O valor da NFD é de: {productA['price']}''')
+                    print(f'''O valor da NFE é de: {productB['price']}''')
+                if productA['unit'] <= productB['unit']:
+                    print(f'''QTD: {productA['unit']} ''')
+                else:
+                    print('A quantidade devolvida é maior')
+                    print(f'''A quantidade comprada foi: {productB['unit']}''')
+                    print(f'''A quantidade devolvida foi: {productA['unit']}''')
     pass
 
 
-
 def start():
-    a = get_doc_ref(get_folder_network(get_cnpj_ref(open_nfd())))
-    get_info_products_nf(open_nfd())
+    a = find_file_xml(open_folder_network(get_cnpj_ref(open_nfd())))
+    b = open_nfe_network(a)
+    cnpj_compare(b)
+    get_products_compare(a)
+    products_compare(get_products_nfd(open_nfd()), get_products_compare(a))
+    get_code_ref(open_nfd())
+    get_products_nfd(open_nfd())
+
     open_nfd().close()
-    open_nfe_compare(a)
 
 
 start()
